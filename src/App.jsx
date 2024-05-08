6e48efe591e02406d67e8456de5e1eb153baebcb
Muhammad Sameer Amjad <proton7ve@gmail.com>
2024-05-08 16:43:31 +0500

    added login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { 
+  BrowserRouter,
+  Routes,
+  Route 
+} from 'react-router-dom'
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
 
 function App() {
 
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
