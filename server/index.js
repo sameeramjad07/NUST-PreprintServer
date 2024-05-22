53f96f83372ad7860255ef59b70ce14fc5f3c489
Muhammad Sameer Amjad <proton7ve@gmail.com>
2024-05-22 13:40:27 +0500

    configuring frontend deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,26 @@ const pool = require('./db')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+
+const ProdOrigin = process.env.ORIGIN
+const devOrigin = ['http://localhost:5173',]
+const allowdOrigin = process.env.NODE_ENV === 'production' ? ProdOrigin : devOrigin
+app.use(cors({
+    origin: (origin, callback) => {
+        if(allowdOrigin.includes(origin) || !origin){
+            console.log('Origin:', origin, "AllowedOrigin: ", allowdOrigin);
+            callback(null, true)
+        }
+        else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
+
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/api/v1', mainRouter)
